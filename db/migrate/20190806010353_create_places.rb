c39abdc4fc597d1ac132ea1d6a7054f1f41bc24e
Joshua Mejia <joshuamejia51@gmail.com>
2019-08-21 00:16:10 +0000

    your message

--- db/migrate/20190806010353_create_places.rb
+++ db/migrate/20190806010353_create_places.rb
@@ -1,15 +1,14 @@
 class CreatePlaces < ActiveRecord::Migration[5.2]
-  def change
-    create_table :places do |t|
+   def change
+    create_table :comments do |t|
       t.text :message
       t.string :rating
       t.integer :user_id
       t.integer :place_id
       t.timestamps
-end 
+    end
 
     add_index :comments, [:user_id, :place_id]
     add_index :comments, :place_id
-
   end
-end 
+end
